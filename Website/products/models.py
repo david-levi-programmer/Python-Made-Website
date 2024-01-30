9ab69d34c441d2903e5eb92fd1eb9f7ac034a432
David Levi <david_r_levi@outlook.com>
2024-01-30 18:32:11 +0000

    Migrating models

--- Website/products/models.py
+++ Website/products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+# 'models.Model' defines all common operations
+# performed on models in Django
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=7)
+    description = models.CharField(max_length=80)
+    discount = models.FloatField()
--- Website/products/urls.py
+++ Website/products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-# all urls on this site start with /products
+# all urls on this site start with /products/
 urlpatterns = [
     path('', views.index),
     path('new', views.new)
