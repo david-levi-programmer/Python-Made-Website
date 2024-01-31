101696049de6d284196bdae54c00bee8427b7613
David Levi <david_r_levi@outlook.com>
2024-01-31 11:12:03 +0000

    Created a reusable HTML template folder and set Django to look there

--- Website/firstwebsite/settings.py
+++ Website/firstwebsite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'firstwebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'Reusable HTML Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
